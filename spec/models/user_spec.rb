76c3441cd4605d8d6739525b0c8475d4fa169da5
Eric Hoff <eric@highfructose.ca>
2025-06-30 20:44:35 -0600

    Add User validation specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,5 +6,23 @@ RSpec.describe User, type: :model do
       user = FactoryBot.build(:user)
       expect(user).to be_valid
     end
+
+    it "should validate presence of email_address" do
+      user = FactoryBot.build(:user, email_address: nil)
+      expect(user).not_to be_valid
+      expect(user.errors[:email_address]).to include("can't be blank")
+    end
+
+    it "should validate uniqueness of email_address" do
+      existing_user = FactoryBot.create(:user)
+      user = FactoryBot.build(:user, email_address: existing_user.email_address)
+      expect(user).not_to be_valid
+      expect(user.errors[:email_address]).to include("has already been taken")
+    end
+
+    it "should normalize email_address" do
+      user = FactoryBot.build(:user, email_address: "TEST@EXAMPLE.COM")
+      expect(user.email_address).to eq("test@example.com")
+    end
   end
 end
