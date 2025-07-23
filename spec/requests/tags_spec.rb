46103138fa93354e55f3ed8410f636146c1c5ad9
Eric Hoff <eric@highfructose.ca>
2025-07-23 09:54:51 -0600

    Fix lint error

--- spec/requests/tags_spec.rb
+++ spec/requests/tags_spec.rb
@@ -13,7 +13,6 @@ require 'rails_helper'
 # sticking to rails and rspec-rails APIs to keep things simple and stable.
 
 RSpec.describe "/tags", type: :request do
-  
   # This should return the minimal set of attributes required to create a valid
   # Tag. As you add validations to Tag, be sure to
   # adjust the attributes here as well.
