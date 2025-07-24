c88042d65caafb4a496879d275f95e911443f23a
Eric Hoff <eric@highfructose.ca>
2025-07-24 07:34:15 -0600

    Brakeman didnt like how I did URLs so added some checks

--- spec/models/bookmark_spec.rb
+++ spec/models/bookmark_spec.rb
@@ -13,6 +13,32 @@ RSpec.describe Bookmark, type: :model do
       expect(bookmark.errors[:url]).to include("can't be blank")
     end
 
+
+    context 'unsafe urls' do
+      it "should not allow invalid URLs" do
+        bookmark = FactoryBot.build(:bookmark, url: "invalid_url")
+        expect(bookmark).not_to be_valid
+        expect(bookmark.errors[:url]).to include("must be a valid HTTP or HTTPS URL")
+      end
+
+      it "should allow valid URLs" do
+        bookmark = FactoryBot.build(:bookmark, url: "http://example.com")
+        expect(bookmark).to be_valid
+      end
+
+      it "should stop javascript URLs" do
+        bookmark = FactoryBot.build(:bookmark, url: "javascript:alert('XSS')")
+        expect(bookmark).not_to be_valid
+        expect(bookmark.errors[:url]).to include("must be a valid HTTP or HTTPS URL")
+      end
+
+      it "should not pass just because http is in the URL" do
+        bookmark = FactoryBot.build(:bookmark, url: "ftp://https.example.com")
+        expect(bookmark).not_to be_valid
+        expect(bookmark.errors[:url]).to include("must be a valid HTTP or HTTPS URL")
+      end
+    end
+
     it "should validate presence of user" do
       bookmark = FactoryBot.build(:bookmark, user: nil)
       expect(bookmark).not_to be_valid
@@ -24,4 +50,5 @@ RSpec.describe Bookmark, type: :model do
       expect(bookmark.url).to eq("http://example.com")
     end
   end
+
 end
