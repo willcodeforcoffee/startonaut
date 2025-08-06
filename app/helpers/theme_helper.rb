ffcda1f8566c8d1ee4425ef7dc1e84ca1df8c55b
Eric Hoff <eric@hoffsoftcorp.com>
2025-08-06 13:44:53 -0600

    Added theming helpers and pages

---
+++ app/helpers/theme_helper.rb
@@ -0,0 +1,16 @@
+module ThemeHelper
+  THEME_LINK_CLASSES = [ "rounded-md", "px-3.5", "py-2.5", "block", "font-medium" ].freeze
+  THEME_LINK_STYLES = {
+    default: [ "bg-slate-500", "hover:bg-slate-600", "text-baby-powder-50" ].freeze,
+    primary: [ "bg-red-violet-700", "hover:bg-red-violet-600", "text-baby-powder-50" ].freeze,
+    danger:  [ "bg-red-cmyk-600", "hover:bg-red-cmyk-500", "text-baby-powder-50" ].freeze
+}.freeze
+
+  def theme_link_to(name, path, options = {})
+    css_classes = THEME_LINK_CLASSES + THEME_LINK_STYLES[options[:style] || :default]
+    options[:class] = [ css_classes, options[:class] ].compact.join(" ")
+    options.delete(:style)
+
+    link_to(name, path, options)
+  end
+end
--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -6,5 +6,10 @@ RSpec.describe "Homes", type: :request do
       get "/home/index"
       expect(response).to have_http_status(:success)
     end
+
+    it "returns http success" do
+      get "/"
+      expect(response).to have_http_status(:success)
+    end
   end
 end
