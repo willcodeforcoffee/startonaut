5deeabe13582dfe84abfe0b2df73ada98c7ff199
Eric Hoff <eric@hoffsoftcorp.com>
2025-08-09 13:47:04 -0600

    Go to pages by default

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,13 @@ class HomeController < ApplicationController
   allow_unauthenticated_access
 
   def index
-    return redirect_to bookmarks_path if authenticated?
+    return redirect_to pages_path if authenticated?
 
     render :index
   end
 
   def theme
+    # Only allow access to the theme page in development
     raise ActionController::RoutingError.new("Not Found") unless Rails.env.development?
   end
 end
