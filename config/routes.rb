14e120b07bcdf3f07d1a505fdc76efcf013e73c4
Eric Hoff <eric@hoffsoftcorp.com>
2025-09-07 22:46:01 -0600

    Make pages the default

--- config/routes.rb
+++ config/routes.rb
@@ -26,5 +26,5 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  root "home#index"
+  root "pages#index"
 end
