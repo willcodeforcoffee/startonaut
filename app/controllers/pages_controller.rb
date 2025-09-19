bdb33ca9ce11ea82be592ecda18c5fad12598bcc
Eric Hoff <eric@highfructose.ca>
2025-09-18 21:54:14 -0600

    Tag list on sidebar  (#44)
    * Show a tags list
    
    * Styling the tag sidebar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def index
-    @bookmarks = Current.user.bookmarks.all
-    @tags = Current.user.tags.all
+    @bookmarks = Current.user.bookmarks.all.order(created_at: :desc)
+    @tags_by_letter = Current.user.tags.order(:name).group_by { |tag| tag.name.first.upcase }
   end
 end
