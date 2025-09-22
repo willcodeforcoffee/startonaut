e19003a0b9eda0e4189230536476e70721e5c8ee
Eric Hoff <eric@hoffsoftcorp.com>
2025-09-21 22:09:19 -0600

    Fix UserFactory to use password/confirmation instead

--- spec/factories/users_factory.rb
+++ spec/factories/users_factory.rb
@@ -1,7 +1,9 @@
 FactoryBot.define do
   factory :user do
     email_address { "test@example.com" }
-    password_digest { BCrypt::Password.create("password") }
+    password { "Password123" }
+    password_confirmation { "Password123" }
+    # password_digest { BCrypt::Password.create("password") }
 
     trait :with_faker_email do
       email_address { Faker::Internet.email }
