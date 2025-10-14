43cbf7feb64aa525ea38a255af59093427eb0320
Eric Hoff <eric@hoffsoftcorp.com>
2025-10-14 17:54:15 -0600

    Fixed up specs

--- spec/requests/account_spec.rb
+++ spec/requests/account_spec.rb
@@ -1,11 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe "Accounts", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/account/index"
-      expect(response).to have_http_status(:success)
+  context "when not logged in" do
+    describe "GET" do
+      it "returns HTTP 302 FOUND" do
+        get "/account"
+        expect(response).to have_http_status(:found)
+      end
     end
   end
 
+  context "when logged in" do
+    let(:authentication_user) { FactoryBot.create(:user) }
+    before(:each) do
+      sign_in_as(authentication_user)
+    end
+
+    describe "GET" do
+      it "returns HTTP 200 OK" do
+        get "/account"
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
 end
