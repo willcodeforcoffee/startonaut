70727511eef6ecdac085418ceb960f73f806a1fa
Eric Hoff <eric@highfructose.ca>
2025-10-29 08:20:00 -0600

    Setup CI so it passes

--- config/ci.rb
+++ config/ci.rb
@@ -9,7 +9,7 @@ CI.run do
   step "Security: Brakeman code analysis", "bin/brakeman --quiet --no-pager --exit-on-warn --exit-on-error"
 
   step "Tests: Rails", "bin/rails test"
-  step "Tests: System", "bin/rails test:system"
+  step "Tests: Rspec", "bin/rspec"
   step "Tests: Seeds", "env RAILS_ENV=test bin/rails db:seed:replant"
 
   # Optional: set a green GitHub commit status to unblock PR merge.
