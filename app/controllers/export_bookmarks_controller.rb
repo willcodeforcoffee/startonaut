b502596e77f7f09824c67416ddad3774671b7976
Eric Hoff <eric@hoffsoftcorp.com>
2025-10-30 17:40:38 -0600

    Export the bookmarks as a downloadable file

--- app/controllers/export_bookmarks_controller.rb
+++ app/controllers/export_bookmarks_controller.rb
@@ -4,6 +4,40 @@ class ExportBookmarksController < ApplicationController
 
   def show
     @bookmarks = Current.user.bookmarks.includes(:tags).order(created_at: :desc)
-    render layout: false
+
+    html_content = generate_netscape_html(@bookmarks)
+
+    send_data html_content,
+              filename: "bookmarks.html",
+              type: "text/html",
+              disposition: "attachment"
+  end
+
+  private
+
+  def generate_netscape_html(bookmarks)
+    <<~HTML
+      <!DOCTYPE NETSCAPE-Bookmark-file-1>
+      <META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">
+      <TITLE>Bookmarks</TITLE>
+      <H1>Bookmarks</H1>
+      <DL>
+        <p>
+          #{bookmarks.map { |bookmark| bookmark_entry(bookmark) }.join("\n")}
+        </p>
+      </DL>
+    HTML
+  end
+
+  def bookmark_entry(bookmark)
+    tags = bookmark.tag_list_no_spaces
+    description = bookmark.description.present? ? "<DD>#{CGI.escapeHTML(bookmark.description)}</DD>" : ""
+
+    <<~ENTRY.strip
+      <DT>
+        <A HREF="#{CGI.escapeHTML(bookmark.url)}" ADD_DATE="#{bookmark.created_at.to_i}" TOREAD="0" PRIVATE="1" TAGS="#{CGI.escapeHTML(tags)}">#{CGI.escapeHTML(bookmark.title)}</A>
+      </DT>
+      #{description}
+    ENTRY
   end
 end
