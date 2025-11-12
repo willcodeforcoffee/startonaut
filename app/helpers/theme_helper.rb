84aa09dcbde8548cb83b038256d586239d986650
Eric Hoff <eric@highfructose.ca>
2025-11-11 22:45:34 -0700

    Homepage redesign (#74)
    * Make buttons fit content
    
    * Style updates to the homepage
    
    * Keep mb consistent

--- app/helpers/theme_helper.rb
+++ app/helpers/theme_helper.rb
@@ -1,5 +1,5 @@
 module ThemeHelper
-  THEME_LINK_CLASSES = [ "rounded-md", "px-4", "py-2", "block", "font-medium" ].freeze
+  THEME_LINK_CLASSES = [ "rounded-md", "px-4", "py-2", "block", "w-fit", "font-medium" ].freeze
   THEME_LINK_STYLES = {
     default: [ "bg-slate-500", "hover:bg-slate-600", "text-baby-powder-50" ].freeze,
     primary: [ "bg-red-violet-700", "hover:bg-red-violet-600", "text-baby-powder-50" ].freeze,
