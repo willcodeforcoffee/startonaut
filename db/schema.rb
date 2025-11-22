f0fcfd59fc42bba7e792d9a2e66e04ee6129e0b9
Eric Hoff <eric@hoffsoftcorp.com>
2025-11-21 20:47:47 -0700

    Run migration, Rails 8.1 sorted columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,26 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_27_144035) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_21_062300) do
   create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
+    t.string "name", null: false
+    t.bigint "record_id", null: false
+    t.string "record_type", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
   create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.string "service_name", null: false
     t.bigint "byte_size", null: false
     t.string "checksum"
+    t.string "content_type"
     t.datetime "created_at", null: false
+    t.string "filename", null: false
+    t.string "key", null: false
+    t.text "metadata"
+    t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
@@ -40,13 +40,13 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_27_144035) do
   end
 
   create_table "bookmarks", force: :cascade do |t|
-    t.string "url", null: false
-    t.string "title"
-    t.text "description"
-    t.integer "user_id", null: false
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.text "description"
     t.string "feed_url"
+    t.string "title"
+    t.datetime "updated_at", null: false
+    t.string "url", null: false
+    t.integer "user_id", null: false
     t.index ["user_id"], name: "index_bookmarks_on_user_id"
   end
 
@@ -55,28 +55,42 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_27_144035) do
     t.integer "tag_id", null: false
   end
 
-  create_table "sessions", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.string "ip_address"
-    t.string "user_agent"
+  create_table "logs", force: :cascade do |t|
     t.datetime "created_at", null: false
+    t.bigint "loggable_id", null: false
+    t.string "loggable_type", null: false
+    t.text "message", null: false
+    t.json "metadata", default: {}
+    t.string "severity"
+    t.string "source"
     t.datetime "updated_at", null: false
+    t.string "user"
+    t.index ["loggable_type", "loggable_id"], name: "index_logs_on_loggable_type_and_loggable_id"
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "ip_address"
+    t.datetime "updated_at", null: false
+    t.string "user_agent"
+    t.integer "user_id", null: false
     t.index ["user_id"], name: "index_sessions_on_user_id"
   end
 
   create_table "tags", force: :cascade do |t|
-    t.string "name", null: false
-    t.integer "user_id", null: false
+    t.boolean "can_delete", default: false
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.boolean "favorite", default: false, null: false
+    t.string "name", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
     t.index ["user_id"], name: "index_tags_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
     t.string "email_address", null: false
     t.string "password_digest", null: false
-    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
