8bf11085952bbe977d2ba629cbc52637c61a86bf
Eric Hoff <eric@highfructose.ca>
2025-12-11 22:53:35 -0700

    Fixed linting issue

--- db/migrate/20251212054437_add_unique_index_to_bookmarks_url.rb
+++ db/migrate/20251212054437_add_unique_index_to_bookmarks_url.rb
@@ -1,5 +1,5 @@
 class AddUniqueIndexToBookmarksUrl < ActiveRecord::Migration[8.1]
   def change
-    add_index :bookmarks, [:url, :user_id], unique: true
+    add_index :bookmarks, [ :url, :user_id ], unique: true
   end
 end
