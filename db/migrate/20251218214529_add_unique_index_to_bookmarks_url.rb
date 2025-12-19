4722ef05898e4c361701648609ae285e5ba34a66
Eric Hoff <eric@highfructose.ca>
2025-12-18 22:31:43 -0700

    Readd index migration (#92)
    * Remove broken Rake task and add index migration
    
    * Update schema after migration

---
+++ db/migrate/20251218214529_add_unique_index_to_bookmarks_url.rb
@@ -0,0 +1,5 @@
+class AddUniqueIndexToBookmarksUrl < ActiveRecord::Migration[8.1]
+  def change
+    add_index :bookmarks, [ :url, :user_id ], unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_12_17_135527) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_18_214529) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
@@ -47,6 +47,7 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_17_135527) do
     t.datetime "updated_at", null: false
     t.string "url", null: false
     t.integer "user_id", null: false
+    t.index ["url", "user_id"], name: "index_bookmarks_on_url_and_user_id", unique: true
     t.index ["user_id"], name: "index_bookmarks_on_user_id"
   end
 
